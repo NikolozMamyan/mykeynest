efa4b2e7975b8691a855123fb00788ffa93e19d5
Nikoloz <nikoloz.mamyan@lnstrade.fr>
2025-04-07 16:44:29 +0200

    token expired redirect to login

--- src/Security/ApiTokenAuthenticator.php
+++ src/Security/ApiTokenAuthenticator.php
@@ -4,15 +4,16 @@ namespace App\Security;
 
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 
 class ApiTokenAuthenticator extends AbstractAuthenticator
 {
@@ -79,6 +80,13 @@ class ApiTokenAuthenticator extends AbstractAuthenticator
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        return new JsonResponse(['error' => 'Unauthorized'], 401);
+        // Si c'est une requête API, on garde une réponse JSON
+        if (str_starts_with($request->getPathInfo(), '/api')) {
+            return new JsonResponse(['error' => $exception->getMessage()], 401);
+        }
+    
+        // Pour une page HTML, on redirige vers une page sympa
+        return new RedirectResponse('/login');
     }
+    
 }
